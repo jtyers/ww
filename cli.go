b1aeea5a3430f11184b2af8f2a460fa45ee3282d
jtyers <jtyers@gmail.com>
2020-06-29 13:29:46 +0100

    feat: move to flaggy for args parsing to get sne behaviour on -h and unknown args

--- cli.go
+++ cli.go
@@ -5,84 +5,83 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/integrii/flaggy"
 	"github.com/jtyers/ww/slice"
-	"github.com/mkideal/cli"
 )
-
-type argT struct {
-	cli.Helper
-
-	Interval int `cli:"interval,n" dft:"2" usage:"run command every X seconds (ignored when --watch is specified)"`
-
-	Shell bool `cli:"shell,s" usage:"run command inside a shell"`
-
-	Highlights []string `cli:"color,colour,c" usage:"highlight specified text in output"`
-}
 
 func parseArgs() WWConfig {
 	config := WWConfig{}
 
+	flInterval := 2
+	flShell := false
+	flHighlights := []string{}
+
+	flaggy.Int(&flInterval, "n", "interval", "Run command every X seconds")
+	flaggy.Bool(&flShell, "s", "shell", "Run command inside a shell (auto-detected via $SHELL)")
+	flaggy.StringSlice(&flHighlights, "c", "color", "Colour (highlight) the given string in output (can be specified multiple times, case-insensitive)")
+
+	flaggy.DefaultParser.ShowVersionWithVersionFlag = true
+	flaggy.DefaultParser.ShowHelpOnUnexpected = true
+	flaggy.DefaultParser.ShowHelpWithHFlag = true
+
 	// Prepend $WW_DEFAULT_ARGS to our args list then process them all as normal
-	args := slice.NewStringSlice([]string{os.Args[0]}).
-		Concat(GetArgsFromEnvironment()).
+	args := slice.NewStringSlice(GetArgsFromEnvironment()).
 		Concat(os.Args[1:]).
 		Value()
+	flaggy.ParseArgs(args)
 
-	res := cli.RunWithArgs(new(argT), args, func(ctx *cli.Context) error {
-		a, _ := ctx.Argv().(*argT)
-
-		//ctx.JSONln(ctx.Argv())
-		//ctx.JSONln(ctx.Args())
-
-		if len(ctx.Args()) == 0 {
-			return fmt.Errorf("command required")
-		}
-
-		config.Command = ctx.Args()[0]
-		config.Args = ctx.Args()[1:]
-
-		if a.Interval > 0 {
-			Interval, err := time.ParseDuration(fmt.Sprintf("%ds", a.Interval))
-			if err != nil {
-				die(nil, "invalid --interval: %v", err)
-			}
-
-			config.Trigger = &IntervalWWTrigger{Interval}
-		}
-
-		if a.Shell {
-			currentShell, ok := os.LookupEnv("SHELL")
-			if !ok {
-				currentShell = "/bin/sh"
-			}
-
-			newArgs := []string{"-c", config.Command}
-
-			for _, arg := range config.Args {
-				newArgs = append(newArgs, arg)
-			}
-
-			config.Command = currentShell
-			config.Args = newArgs
-		}
-
-		highlights := map[string]string{}
-		if len(a.Highlights) > 0 {
-			for _, highlight := range a.Highlights {
-				highlights[highlight] = "[red]"
-			}
-		}
-
-		config.Highlighter = NewHighlighter(highlights) // always set this so it is not nil
-
-		return nil
-	})
-
-	if res != 0 {
-		os.Exit(res)
+	if len(flaggy.TrailingArguments) == 0 {
+		flaggy.ShowHelpAndExit("command required")
 	}
+
+	config.Command = flaggy.TrailingArguments[0]
+	config.Args = flaggy.TrailingArguments[1:]
+
+	if flInterval > 0 {
+		Interval, err := time.ParseDuration(fmt.Sprintf("%ds", flInterval))
+		if err != nil {
+			die(nil, "invalid --interval: %v", err)
+		}
+
+		config.Trigger = &IntervalWWTrigger{Interval}
+	}
+
+	if flShell {
+		currentShell, ok := os.LookupEnv("SHELL")
+		if !ok {
+			currentShell = "/bin/sh"
+		}
+
+		newArgs := []string{
+			"-c",
+			strings.Join(
+				slice.ConcatString([]string{config.Command}, slice.MapString(config.Args, func(s string, n int) string {
+					// for every arg, if it contains whitespace, enclose it in quotes
+					if strings.Contains(s, " ") || strings.Contains(s, "\t") {
+						return "\"" + s + "\""
+					}
+
+					return s
+				})),
+				" ",
+			),
+		}
+
+		config.Command = currentShell
+		config.Args = newArgs
+	}
+
+	highlights := map[string]string{}
+	if len(flHighlights) > 0 {
+		for _, highlight := range flHighlights {
+			highlights[highlight] = "[red]"
+		}
+	}
+
+	config.Highlighter = NewHighlighter(highlights) // always set this so it is not nil
 
 	fmt.Printf("config: %#v\n", config)
 	return config
