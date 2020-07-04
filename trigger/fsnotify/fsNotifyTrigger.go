839124e548c224e0aca894c75c5b0a2e016d84dc
jtyers <jtyers@gmail.com>
2020-07-04 23:04:36 +0100

    fix: do not re-use watchers, re-create every time;

--- trigger/fsnotify/fsNotifyTrigger.go
+++ trigger/fsnotify/fsNotifyTrigger.go
@@ -8,59 +8,69 @@ import (
 )
 
 type FsNotifyTrigger struct {
-	watcher *fsnotify.Watcher
+	directory    string
+	excludeNames []string
 }
 
 func NewFsNotifyTrigger(directory string, excludeNames []string) (*FsNotifyTrigger, error) {
+	trigger := FsNotifyTrigger{directory, excludeNames}
+	return &trigger, nil
+}
+
+func (t *FsNotifyTrigger) WaitForTrigger(interruptChan <-chan error) (<-chan bool, <-chan string) {
+	// Since fsnotify's API only allows you to create a watcher (and that watcher then
+	// imediately starts watching stuff), we have to create a fresh watcher every time
+	// we wait for the trigger, and also do the related directory walking, since you cannot
+	// ever re-use a watcher.
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		return nil, fmt.Errorf("could not create watcher: %v", err)
+		panic(fmt.Errorf("could not create watcher: %v", err))
 	}
 
-	paths, err := dirwalk.WalkDirectory(directory, excludeNames, nil, true)
+	paths, err := dirwalk.WalkDirectory(t.directory, t.excludeNames, nil, true)
 	if err != nil {
-		return nil, fmt.Errorf("walk: %v", err)
+		//return nil, fmt.Errorf("walk: %v", err)
+		return nil, nil
 	}
 
 	for _, path := range paths {
 		watcher.Add(path)
 	}
 
-	trigger := FsNotifyTrigger{watcher}
-	return &trigger, nil
-}
-
-func (t *FsNotifyTrigger) WaitForTrigger(interruptChan <-chan error) (<-chan bool, <-chan string) {
 	triggerChan := make(chan bool)
 
 	// Must be buffered.
 	statusChan := make(chan string, 10)
 
 	go func() {
+		defer close(triggerChan)
+		defer close(statusChan)
+
 		statusChan <- "waiting for changes..."
 		for {
 			select {
-			case event, ok := <-t.watcher.Events:
+			case event, ok := <-watcher.Events:
 				if !ok {
 					continue
 				}
 
 				if event.Op&fsnotify.Write == fsnotify.Write || event.Op == fsnotify.Create || event.Op == fsnotify.Remove || event.Op == fsnotify.Rename || event.Op == fsnotify.Chmod {
+					fmt.Printf("change detected in %v\n", event.Name)
 					triggerChan <- true
-					break
+					watcher.Close() // close watcher and quit goroutine
+					return
 				}
 
-			case err, ok := <-t.watcher.Errors:
+			case err, ok := <-watcher.Errors:
 				if !ok {
 					statusChan <- fmt.Sprintf("watch error (%v)", err)
 				}
 
 			case _ = <-interruptChan:
-				t.watcher.Close() // close watcher and quit goroutine
-				break
+				watcher.Close() // close watcher and quit goroutine
+				return
 			}
 		}
-
 	}()
 
 	return triggerChan, statusChan
